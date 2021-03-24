73b755fec12206acc19230ce2940b9936c710a0c
DaniLllera <dlleraa01@elpuig.xeill.net>
2021-03-24 13:57:00 +0100

    prueba1

---
+++ src/test/java/ex2/HashTableTest.java
@@ -0,0 +1,32 @@
+package ex2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HashTableTest {
+
+    @Test
+    void put() {
+        HashTable hashTable = new HashTable();
+
+        hashTable.put("1","1");
+        System.out.println(hashTable.toString());
+
+
+        hashTable.put("2","2");
+        System.out.println(hashTable.toString());
+
+        hashTable.put("13","Hola");
+        System.out.println(hashTable.toString());
+        System.out.println("Numero que genera el has code:" + " " +"13".hashCode());
+
+        Assertions.assertEquals("13".hashCode() % hashTable.size(), 2);
+
+        hashTable.put("24","QUE");
+        System.out.println(hashTable.toString());
+        System.out.println("Numero que genera el has code:" + " " +"24".hashCode());
+        Assertions.assertEquals("24".hashCode() % hashTable.size(), 2);
+
+
+    }
+}
